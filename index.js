627c32c151258466fe184d05f994ababa2301788
Haofeng Feng <fenghaofeng@live.com>
2023-02-19 18:18:29 -0800

    template index.js added

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+import {DynamoDBClient, ScanCommand} from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({});
+const input = {TableName: `ubc-course-sub-${process.env.STAGE}-course`};
+const command = new ScanCommand(input);
+
+export const handler = async (event) => {
+    const response = await client.send(command);
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "Go Serverless v3.0! Your function executed successfully!",
+                input: event,
+                response: response
+            },
+            null,
+            2
+        ),
+    };
 };
